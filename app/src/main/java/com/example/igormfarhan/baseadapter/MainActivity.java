e9da0d1fc8f72e7cc94480a639cf669a28027de4
Bima Tri P <satya24@student.uns.ac.id>
2016-06-11 08:01:35 +0700

    saveeee

--- app/src/main/java/com/example/igormfarhan/baseadapter/MainActivity.java
+++ app/src/main/java/com/example/igormfarhan/baseadapter/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.igormfarhan.baseadapter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity  {
     ListView listMahasiswa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,15 @@ public class MainActivity extends Activity{
         AdapterMahasiswa myAdapter = new AdapterMahasiswa(this);
 
         listMahasiswa.setAdapter(myAdapter);
+
+        listMahasiswa.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(),"save",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
+
 }
